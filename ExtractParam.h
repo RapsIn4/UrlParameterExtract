b2cf08714b6af4e8b47fbf30bc8d15e0c71894ea
Michael Ye <m6ye@uwaterloo.ca>
2014-07-18 10:50:20 -0400

    initial commit

---
+++ ExtractParam.h
@@ -0,0 +1,13 @@
+//
+//  ExtractParam.h
+//  Mom's App
+//
+//  Created by Michael Ye on 2014-07-11.
+//  Copyright (c) 2014 IdeaNotion. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExtractParam : NSString
+- (id)initWithUrl :(NSString *)url withParam:(NSString *)param;
+@end
---
+++ ExtractParam.m
@@ -0,0 +1,39 @@
+//
+//  ExtractParam.m
+//  Mom's App
+//
+//  Created by Michael Ye on 2014-07-11.
+//  Copyright (c) 2014 IdeaNotion. All rights reserved.
+//
+
+#import "ExtractParam.h"
+
+@implementation ExtractParam
+- (id)initWithUrl :(NSString *)url withParam:(NSString *)param
+{
+    self = [super init];
+    if (self)
+    {
+        NSRange pos = [url rangeOfString:param];
+        
+        if (pos.location == NSNotFound) {
+            return @"";
+        }
+        
+        NSString *query = [url substringFromIndex:pos.location+[param length]+1];
+        
+        
+        pos = [query rangeOfString:param];
+        
+        NSUInteger end = [query rangeOfString:@"&"].location;
+        
+        if (end == NSNotFound) {
+            return query;
+        } else {
+            return [query substringWithRange:NSMakeRange(0,end)];
+        }
+        
+    }
+    return self;
+}
+@end
